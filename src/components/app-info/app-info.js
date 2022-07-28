93a5668fcc3227c2daf8de33a0b3f9b400effc28
Rustam <rustam.zholdubayev@gmail.com>
2022-07-28 13:45:28 -0400

    136 practice. updating state

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({increased, employees}) => {
     return (
         <div className='app-info'>
             <h1>Accounting of employees in the company N</h1>
-            <h2>Total number of employees: </h2>
-            <h2>Bonus received: </h2>
+            <h2>Total number of employees: {employees}</h2>
+            <h2>Bonus received: {increased}</h2>
         </div>
     )
 }
