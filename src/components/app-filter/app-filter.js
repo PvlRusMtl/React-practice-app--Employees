13f13f0ce2aa78062e05816e564bf069ea35dde4
Rustam <rustam.zholdubayev@gmail.com>
2022-08-01 11:55:51 -0400

    142 dynamic classes ans styles

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,19 +2,21 @@ import './app-filter.css';
 
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: 'all', label: 'All employees'},
-        {name: 'rise', label: 'Employees for promotion'},
-        {name: 'moreThan3000', label: 'Salary more than 3000$'},
+        {name: 'all', label: 'All employees', colored: false},
+        {name: 'rise', label: 'Employees for promotion', colored: false},
+        {name: 'moreThan3000', label: 'Salary more than 3000$', colored: true},
     ];
 
-    const buttons = buttonsData.map(({name, label}) => {
+    const buttons = buttonsData.map(({name, label, colored}) => {
         const active = props.filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light'
+        const style = colored ? {color: 'red'} : null;
         return (
             <button className={`btn ${clazz}`}
                 type='button'
                 key={name}
-                onClick={() => props.onFilterSelect(name)}>
+                onClick={() => props.onFilterSelect(name)}
+                style={style}>
                 {label}
             </button>  
         )
